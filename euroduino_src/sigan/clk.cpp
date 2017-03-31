37aebe0b58f23b03875f0e4a9752459303bca0a1
Vincent Dupre <vdupre@etri.re.kr>
2017-03-31 18:16:42 +0900

    code cleaning, to be tested

--- euroduino_src/sigan/clk.cpp
+++ euroduino_src/sigan/clk.cpp
@@ -24,13 +24,6 @@ clk::clk(){
 clk::~clk(){
 }
 
-void clk::clk_sync_extern(){
-	_ms = _elapsed_ms;
-	_elapsed_ms = 0;
-	_bpm = ms_to_bpm(_ms);
-	_step_cnt = (_step_cnt + 1) % _max_step;
-}
-
 void clk::clk_sync_intern(uint32_t ms){
 	if(_operation < 0){
 		_ms = ms * abs(_operation);
@@ -101,22 +94,6 @@ boolean clk::clk_set_operation(int op, uint32_t ms_ref){
 	return ret;
 }
 
-boolean clk::clk_next_step(){
-	boolean ret = false;
-	if(_resync){
-		_resync = false;
-		_elapsed_ms = 0;
-		_step_cnt = (_step_cnt + 1) % _max_step;
-		ret = true;
-	}
-	else if(_elapsed_ms > _ms){
-		_elapsed_ms = 0;
-		_step_cnt = (_step_cnt + 1) % _max_step;
-		ret = true;
-	}
-	return ret;
-} 
-
 uint32_t clk::clk_sync_divider(uint32_t ms, uint16_t step){
 	uint8_t divider = abs(_operation);
 	uint32_t ret = 0;
@@ -136,8 +113,6 @@ uint32_t clk::clk_sync_multiplier(uint32_t ms){
 	_ms = ms / _operation;
 	_bpm = ms_to_bpm(_ms);
 	_elapsed_ms = 0;
-
-//	_resync = true;
 	return _ms;
 }
 
@@ -145,16 +120,6 @@ uint32_t clk::clk_reset(){
 	_elapsed_ms = 0;
 	_step_cnt = 0;
 	return _ms;
-}
-
-void clk::clk_sync_slaved(uint16_t step){
-	if(_operation < 0){
-		if( (step % abs(_operation)) == 0 )
-			_resync = true;
-	}
-	else {
-		_resync = true;
-	}
 }
 
 uint32_t clk::clk_sync(uint32_t ms, uint16_t step){
@@ -189,40 +154,6 @@ uint32_t clk::master_sync(uint32_t mst_ms, uint16_t mst_cnt){
 	/* when _step_cnt = _operation, slave clk has to be synced with master */
 	else if( (_operation > 0) && (_step_cnt < (_operation - 1)) ){
 		ret = clk_elapsed(); 
-	}
-	
-	return ret;
-}
-
-boolean clk::clk_update(boolean sync_flg, clk* master){
-	uint8_t divider = abs(_operation);	
-	boolean ret = false;
-	uint32_t ms = master->clk_get_ms();
-	uint16_t step = master->clk_get_step_cnt();
-	
-	// sync forced by master
-	if(sync_flg){
-		if(_operation < 0){
-			_ms = ms * divider;
-			_bpm = ms_to_bpm(_ms);
-			if( (step % divider) == 0 ){
-				_elapsed_ms = 0;
-				_step_cnt = (_step_cnt + 1) % _max_step;
-				ret = true;
-			}
-		} else {
-			_ms = ms / _operation;
-			_bpm = ms_to_bpm(_ms);
-			_elapsed_ms = 0;
-			_step_cnt = (_step_cnt + 1) % _max_step;
-			ret = true;
-		}
-	}
-	// or sync because of timeup
-	else if(_elapsed_ms > _ms){
-		_elapsed_ms = 0;
-		_step_cnt = (_step_cnt + 1) % _max_step;
-		ret = true;
 	}
 	return ret;
 }
