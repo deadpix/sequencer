1dfa6c874d9b57e88d35a7d0d4093d06e9bf193b
vincent <vdupre@etri.re.kr>
2018-05-25 17:59:15 +0900

    add long push handler and menu/param button handler. compile ?? exec ??

--- seq_emulator/mainwindow.cpp
+++ seq_emulator/mainwindow.cpp
@@ -13,6 +13,40 @@ const char* cyan = "background-color: cyan";
 const char* white = "background-color: white";
 
 static elapsedMillis ms;
+static gui oled_gui;
+static gui *gui_ctrl;
+static sequenception sequenception;
+
+static void tempo_change_handler(uint32_t ms){
+	qDebug("tempo_change_handler ms=%d",ms);
+}
+static void midi_note_on(uint16_t note, uint8_t vel, uint8_t chan){
+	qDebug("midi_note_on note=%d vel=%d chan=%d",note,vel,chan);
+}
+
+static void refresh_oled(char** line_arr){
+	qDebug("OLED[0]: %s",line_arr[0]);
+	qDebug("OLED[1]: %s",line_arr[1]);
+	qDebug("OLED[2]: %s",line_arr[2]);
+}
+
+#define BTN_RELEASED	0
+#define BTN_PUSHED	1
+#define BTN_LONG_PUSHED	2
+#define LONG_PRESS_TIME_MS	1000
+
+static void init_menu_btn(prog* p){
+	sequenception.menu_ctrl.set_next_prog(p);
+}
+
+void MainWindow::setup(){
+	gui_ctrl = setup_oled();
+	sequenception.fct_midi = midi_note_on;
+	sequenception.fct_tempo_change = tempo_change_handler;
+	sequenception.init(sequenception.current_prog);
+
+}
+
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent){
 	// Create the button, make "this" the parent
@@ -30,10 +64,15 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent){
 		matrix_btn[i]->setGeometry(QRect(QPoint(x, y),
 					QSize(btn_size, btn_size)));
 //		matrix_btn[i]->setStyleSheet(white);
-		connect(matrix_btn[i], SIGNAL (released()), &mapper, SLOT (map()));
-		mapper.setMapping(matrix_btn[i], i);
+		connect(matrix_btn[i], SIGNAL (released()), &mapper_release, SLOT (map()));
+		connect(matrix_btn[i], SIGNAL (pressed()), &mapper_press, SLOT (map()));
+		mapper_press.setMapping(matrix_btn[i], i);
+		mapper_release.setMapping(matrix_btn[i], i);
+	
+		btn_status[i] = BTN_RELEASED;
 	}
-	connect(&mapper, SIGNAL(mapped(int)), this, SLOT(handleButton(int)));
+	connect(&mapper_press, SIGNAL(mapped(int)), this, SLOT(handleButtonPress(int)));
+	connect(&mapper_release, SIGNAL(mapped(int)), this, SLOT(handleButtonRelease(int)));
 
 	ui_timer = new QTimer(this);
 	connect(ui_timer, SIGNAL(timeout()), this, SLOT(handleTimerUI()));
@@ -44,7 +83,6 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent){
 	connect(loop_timer, SIGNAL(timeout()), this, SLOT(handleMainLoop()));
 	loop_timer->start(10);
 
-
 	menu_btn = new QPushButton("menu", this);
 	menu_btn->setGeometry(QRect(QPoint(pos_x, 60 * 9), QSize(50, 50)));
 
@@ -53,6 +91,9 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent){
 
 	connect(menu_btn, SIGNAL (released()), this, SLOT (handleParamBtn()));
 	connect(param_btn, SIGNAL (released()), this, SLOT (handleMenuBtn()));
+
+	menu_btn_status = BTN_RELEASED;
+	param_btn_status = BTN_RELEASED;
 
 	ms = 0;
 
@@ -76,18 +117,65 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent){
 }
 
 void MainWindow::handleParamBtn(){
-	qDebug ("handle param button");
+
+	if(!param_btn_status){
+		param_btn->setStyleSheet(red);
+		param_btn_status = BTN_PUSHED;
+		menu_btn->setEnabled(false);
+		qDebug ("handle param button");
+	} else {
+		param_btn->setStyleSheet(white);
+		param_btn_status = BTN_RELEASED;
+		menu_btn->setEnabled(true);
+		qDebug ("param menu button");
+	}
 }
 
 void MainWindow::handleMenuBtn(){
-	qDebug ("handle menu button");
+
+	if(!menu_btn_status){
+		menu_btn->setStyleSheet(red);
+		menu_btn_status = BTN_PUSHED;
+		param_btn->setEnabled(false);
+		qDebug ("handle menu button");
+	} else {
+		menu_btn->setStyleSheet(white);
+		menu_btn_status = BTN_RELEASED;
+		param_btn->setEnabled(true);
+		qDebug ("release menu button");
+	}
+	
 }
 
-void MainWindow::handleButton(int id)
+void MainWindow::handleButtonPress(int id)
 {
-	unsigned long long tmp = ms;
-	qDebug ("click on btn %d: ms=%d",id,tmp);
-	ms = 0;
+	qDebug ("press on btn %d",id);
+	btn_status[id] = BTN_LONG_PUSH;
+	btn_ms[i].btn_ms = 0;
+}
+void MainWindow::handleButtonRelease(int id)
+{
+	if(btn_status[id] == BTN_LONG_PUSHED){
+		qDebug("long release %d",id);
+	}
+	else if(btn_status[id] == BTN_PUSHED){
+		qDebug("release %d",id); 
+	} 
+	else {
+		qDebug("Fatal error in handleButtonRelease for button %d!!!!!",id);
+	}
+	btn_status[id] = BTN_RELEASED;
+}
+
+static void check_matrix_btn(struct _btn_status status[MATRIX_NR_BTNS], elapsedMillis ms[MATRIX_NR_BTNS]){
+	for(int i=0;i<MATRIX_NR_BTNS;i++){
+		if(status[i] == BTN_PUSHED){
+			if(ms[i] > LONG_PRESS_TIME_MS){
+				status[i] == BTN_LONG_PUSHED;
+				qDebug ("long press on btn %d",id);
+			}
+		}
+	}
 }
 
 static void upd_btn_color_row(uint16_t* bmp_ret, uint16_t bmp, const char* color, int row_id, QPushButton *matrix_btn[MATRIX_NR_BTNS]){
@@ -147,6 +235,7 @@ void MainWindow::handleTimerUI(){
 //	matrix_btn[step_cnt]->setStyleSheet(red);
 	
 	upd_btn_color(&lm, matrix_btn);
+	check_matrix_btn(btn_status, btn_ms);
 }
 void MainWindow::handleMainLoop(){
 }
