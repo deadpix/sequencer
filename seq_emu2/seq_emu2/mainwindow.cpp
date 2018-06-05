d98505014b13a0fca68cc0b73d036fedcff22632
vincent <vdupre@etri.re.kr>
2018-06-05 14:26:05 +0900

    sequenception emulator: GUI fix

--- seq_emu2/seq_emu2/mainwindow.cpp
+++ seq_emu2/seq_emu2/mainwindow.cpp
@@ -5,7 +5,63 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
+	QString str = "";
+	bool fail = false;
+
+	for(int i=0;i<MATRIX_NR_BTN;i++){
+		str = QString("pushButton_%1").arg(i);
+		btnMatrix[i] = this->findChild<QPushButton *>(str);
+		if(!btnMatrix[i]){
+			qDebug("unable to find btn %s...",str.toUtf8().constData());
+			fail = true;
+		} 
+		else {
+			connect(btnMatrix[i], SIGNAL (released()), &signalMatrixMapperRelease, SLOT (map()));
+			connect(btnMatrix[i], SIGNAL (pressed()), &signalMatrixMapperPress, SLOT (map()));
+			signalMatrixMapperPress.setMapping(btnMatrix[i], i);
+			signalMatrixMapperRelease.setMapping(btnMatrix[i], i);			
+		}
+	}
+	
+	if(!fail){
+		connect(&signalMatrixMapperPress, SIGNAL(mapped(int)), this, SLOT(handleBtnPress(int)));
+		connect(&signalMatrixMapperRelease, SIGNAL(mapped(int)), this, SLOT(handleBtnRelease(int)));
+	}
+	
+	uiTimer = new QTimer(this);
+	connect(uiTimer, SIGNAL(timeout()), this, SLOT(handleTimerUI()));
+	uiTimer->start(100);
+
+	for(int i=0;i<OLED_LINE;i++){
+		str = QString("oled_%1").arg(i);
+		oled[i] = this->findChild<QLabel *>(str);
+		
+		if(!oled[i]){
+			qDebug("unable to find label %s...",str.toUtf8().constData());
+		}
+	}
+
+	btnMenu = this->findChild<QPushButton *>("menu");
+	if(!btnMenu){
+		qDebug("unable to find menu button %s...",str.toUtf8().constData());
+		fail = true;
+	}
+	else {
+		connect(btnMenu, SIGNAL (released()), this, SLOT (handleMenuBtn()));
+	}
+		
+	btnParam = this->findChild<QPushButton *>("param");
+	if(!btnParam){
+		qDebug("unable to find parameter button %s...",str.toUtf8().constData());
+		fail = true;
+	}
+	else {
+		connect(btnParam, SIGNAL (released()), this, SLOT (handleParamBtn()));
+	}
+	
+
+
 }
 
 MainWindow::~MainWindow()
@@ -13,9 +69,18 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::testHandleBtn(){
-    qDebug("TEST");
+void MainWindow::handleBtnPress(int btn_id){
+	qDebug("btn%d pressed", btn_id);
 }
-void MainWindow::slot1(){
-    qDebug("TEST");
+void MainWindow::handleBtnRelease(int btn_id){
+	qDebug("btn%d released", btn_id);
+}
+void MainWindow::handleTimerUI(){
+//	qDebug("timer expired");
+}
+void MainWindow::handleParamBtn(){
+	qDebug("handle parameter button");
+}
+void MainWindow::handleMenuBtn(){
+	qDebug("handle menu button");
 }
--- seq_emu2/seq_emu2/mainwindow.h
+++ seq_emu2/seq_emu2/mainwindow.h
@@ -2,6 +2,16 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSignalMapper>
+#include <QPushButton>
+#include <QLabel>
+#include <QtDebug>
+#include <QTimer>
+
+#define MATRIX_NR_COL	8
+#define MATRIX_NR_ROW	8
+#define MATRIX_NR_BTN	(MATRIX_NR_COL * MATRIX_NR_ROW)
+#define OLED_LINE	3
 
 namespace Ui {
 class MainWindow;
@@ -9,18 +19,31 @@ class MainWindow;
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
 
 private slots:
-    void testHandleBtn();
-	void slot1();
+	void handleBtnPress(int);
+	void handleBtnRelease(int);
+	void handleTimerUI();
+	void handleParamBtn();
+	void handleMenuBtn();
 
 private:
-    Ui::MainWindow *ui;
+	Ui::MainWindow *ui;
+
+	QSignalMapper signalMatrixMapperPress;
+	QSignalMapper signalMatrixMapperRelease;
+	
+	QTimer *uiTimer;	
+	
+	QPushButton* btnMatrix[MATRIX_NR_BTN];
+	QPushButton* btnMenu;
+	QPushButton* btnParam;
+	QLabel*	     oled[OLED_LINE];
 };
 
 #endif // MAINWINDOW_H
