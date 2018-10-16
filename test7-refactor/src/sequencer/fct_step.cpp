8f6a2c4ec574a2fb5292999069feffe4d5dff6e6
vincent <vdupre@gmail.com>
2018-10-16 23:27:17 +0900

    Fix GUI problem. clock calculation for sub step does not work

--- test7-refactor/src/sequencer/fct_step.cpp
+++ test7-refactor/src/sequencer/fct_step.cpp
@@ -16,131 +16,11 @@ static void clear_all_long_pushed_ui(track* t, uint8_t* lp_cnt, struct led_blink
 	int ret = 0;
 	for(uint8_t i=0; i<*lp_cnt; i++){
 		ret = t->get_led_matrix()->clr_n_restore(lp_ui[i]._id, FOREGROUND1);
-		dbg::printf("clear %d ret=%d \n",lp_ui[i]._id,ret);
+//		dbg::printf("clear %d ret=%d \n",lp_ui[i]._id,ret);
 	}
 	*lp_cnt = 0;
 
 }
-
-//static bool btn_was_long_pushed(uint8_t btn_id, uint8_t lp_cnt, const struct led_blink_t* lp_ui){
-//	bool flg = false;
-//	for(uint8_t i=0; i<lp_cnt; i++){
-//		if(btn_id == lp_ui[i]._id){
-//			flg = true;
-//		}
-//	}
-//	return flg;
-//}
-
-//static bool link_steps(sequencer *s, uint8_t first, uint8_t second){
-//	track* t = s->get_current_track();
-//	uint8_t step_cnt = first;
-//	bool res = false;
-/*
-	// check first if previous step already linked
-	uint8_t prev_step;
-	if(step_cnt == 0){
-		prev_step = t->get_max_step();
-	} else {
-		prev_step = step_cnt - 1;
-	}
-
-		if(!t->_mtx_btn_to_step[prev_step]->is_step_linked()){
-			t->_mtx_btn_to_step[step_cnt]->set_step_up();
-			t->_mtx_btn_to_step[step_cnt]->link_step();
-
-			t->get_led_matrix()->save_n_set(t->_mtx_btn_to_step[step_cnt]->get_step_color(), errata_step[step_cnt], BACKGROUND);
-			res = true;
-			step_cnt = (step_cnt + 1) % t->get_max_step();
-		}	
-
-	while(step_cnt != second){
-		t->_mtx_btn_to_step[step_cnt]->set_step_off();
-		t->_mtx_btn_to_step[step_cnt]->link_step();
-		t->get_led_matrix()->save_n_set(t->_mtx_btn_to_step[step_cnt]->get_step_color(), errata_step[step_cnt], BACKGROUND);
-		step_cnt = (step_cnt + 1) % t->get_max_step();
-	}
-	
-	// check if step after "second" is linked 
-	if(!t->_mtx_btn_to_step[(step_cnt + 1) % t->get_max_step()]->is_step_linked()){
-		t->_mtx_btn_to_step[step_cnt]->set_step_dw();
-		t->_mtx_btn_to_step[step_cnt]->link_step();
-		t->get_led_matrix()->save_n_set(t->_mtx_btn_to_step[step_cnt]->get_step_color(), errata_step[step_cnt], BACKGROUND);
-	}
-	else {
-		t->_mtx_btn_to_step[step_cnt]->set_step_off();
-		t->_mtx_btn_to_step[step_cnt]->link_step();
-		t->get_led_matrix()->save_n_set(t->_mtx_btn_to_step[step_cnt]->get_step_color(), errata_step[step_cnt], BACKGROUND);
-	}
-*/
-//	return res;
-//}
-
-//static void unlink_steps(sequencer *s, uint8_t step){
-//	track* t = s->get_current_track();
-//	uint8_t tmp;
-/*	
-	//TODO reevaluate linked step
-
-	if(step == 0){
-		tmp = t->get_max_step();	
-	} else {
-		tmp = step - 1;
-	}
-
-
-	if(t->_mtx_btn_to_step[tmp]->is_step_active()){
-		t->_mtx_btn_to_step[tmp]->set_step_dw();
-	}
-	tmp = (step + 1) % t->get_max_step();
-	if(!t->_mtx_btn_to_step[tmp]->is_step_active()){
-		t->_mtx_btn_to_step[tmp]->set_step_up();
-	}
-*/
-//}
-
-
-//static void shift_step_ui(track* t, step* s, int shift){
-//	int ret;
-//	uint8_t id = s->_step_ui_id + shift;
-////	Serial.print("shift_step_ui ");
-////	Serial.println(id);	
-//
-//	if( id >= NR_STEP ){
-//		id = NR_STEP - 1;
-//		t->_mtx_btn_to_step[id] = NULL;
-//	}
-//	else {
-////		t->get_led_matrix()->clr_n_restore(errata_step[id], BACKGROUND);	
-//		t->_mtx_btn_to_step[id] = s;
-//		if(s->is_step_active()){
-////			Serial.print("active step ");
-////			Serial.println(errata_step[id]);
-//			ret = t->get_led_matrix()->save_n_set(s->get_step_color(), errata_step[id], BACKGROUND);
-////			if(ret < 0)
-////				Serial.println("Fail to save_n_set");
-//		}
-//	}
-//	s->_step_ui_id = id;
-//	
-//}
-
-
-//static void clear_steps(step* step_from, step* step_to, track* t){
-//	step* s = t->_mtx_btn_to_step[from]->get_next_step();
-/*
-	step* s = step_from;
-	while(s != step_to){
-		// clear ui
-		t->get_led_matrix()->clr_n_restore(errata_step[s->_step_ui_id],BACKGROUND);	
-
-		// delete step
-		step* tmp = s->get_next_step();
-		track::delete_step(&t->_step_list,s);
-		s = tmp;
-	}
-*/
-//}
 
 void fct_step::on_push(uint8_t btn_id){
 //	char str[7];
@@ -174,74 +54,73 @@ void fct_step::on_release(uint8_t btn_id){
 	
 	t->get_led_matrix()->clr_n_restore(btn_id, FOREGROUND1);
 	
-	if(n && n->_step){
 
 	// check if the pushed button was long pushed button
-		if(_lp_cnt == 1){
-			// linked step
-			/* Temporarily disabled */
-		   	/*	
-			if(!btn_was_long_pushed(btn_id, _lp_cnt, _lp_ui)){
-				if(link_steps(_seq, errata_btn[_lp_ui[_lp_cnt-1]._id], errata_btn[btn_id])){
-					_seq->_ls_ui.ms_cnt = 0;
-					_seq->_ls_ui.step_id = _lp_ui[_lp_cnt-1]._id;
+	if(_lp_cnt == 1){
+		// linked step
+		/* Temporarily disabled */
+	   	/*	
+		if(!btn_was_long_pushed(btn_id, _lp_cnt, _lp_ui)){
+			if(link_steps(_seq, errata_btn[_lp_ui[_lp_cnt-1]._id], errata_btn[btn_id])){
+				_seq->_ls_ui.ms_cnt = 0;
+				_seq->_ls_ui.step_id = _lp_ui[_lp_cnt-1]._id;
+			}
 				}
-	
-			}
-			*/
-			clear_all_long_pushed_ui(t, &_lp_cnt, _lp_ui);
-		} 
-		else if(_lp_cnt == 2){
-			// TODO create substep
-//			s->_clk_def.numerator = (to-from);
-//			s->_clk_def.denominator = nr_new_step;	
-			uint8_t len = (id % t->get_max_step()) + 1;
-			
-			// TODO: first_step might be deleted
-//			node* tmp = t->get_first_step()->_node;
-			node* start = t->get_node_from_matrix(errata_btn[_lp_ui[0]._id]);
-			node* end = t->get_node_from_matrix(errata_btn[_lp_ui[1]._id]);
-			node* parent = start->_parent;
-
+		*/
+		clear_all_long_pushed_ui(t, &_lp_cnt, _lp_ui);
+	} 
+	else if(_lp_cnt == 2){
+		// TODO create substep
+//		s->_clk_def.numerator = (to-from);
+//		s->_clk_def.denominator = nr_new_step;	
+		uint8_t len = (id % t->get_max_step()) + 1;
+		
+		// TODO: first_step might be deleted
+//		node* tmp = t->get_first_step()->_node;
+		node* start = t->get_node_from_matrix(errata_btn[_lp_ui[0]._id]);
+		node* end = t->get_node_from_matrix(errata_btn[_lp_ui[1]._id]);
+		node* parent = start->_parent;
 			// delete node between start and end
-			for(uint8_t i=(start->_node_id+1);i<end->_node_id;i++){
-				node* tmp = parent->_children->remove(i);
-				t->set_node_in_matrix(tmp->_mtx_id, NULL);
-				// should we delete node???
-				delete tmp;
-			}
-			// delete step of start node
-			delete start->_step;
-			start->_step = NULL;
+		for(uint8_t i=(start->_node_id+1);i<end->_node_id;i++){
+			node* tmp = parent->_children->remove(i);
+			t->set_node_in_matrix(tmp->_mtx_id, NULL);
+			// should we delete node???
+			delete tmp;
+		}
+		uint8_t new_numerator = start->_step->_clk_def.numerator * (end->_node_id - start->_node_id); 
+		uint8_t new_denominator = start->_step->_clk_def.denominator * len; 
+		// delete step of start node
+		delete start->_step;
+		start->_step = NULL;
 
-			// create x nodes and steps
-			t->create_tree(start, len, (end->_node_id - start->_node_id), 
-					len, start->_node_lvl * DEFAULT_STEP_PER_SEQ);
-			// chain steps
-			track::chain_step_from_node_list(start->_children,
-						  start->_children->get(0)->_step,
-						  start->_children->get(len-1)->_step);
+		// create x nodes and steps
+		t->create_tree(start, len, new_numerator, 
+			new_denominator, start->_node_lvl * DEFAULT_STEP_PER_SEQ);
+		// chain steps
+		track::chain_step_from_node_list(start->_children,
+				  start->_children->get(0)->_step,
+				  start->_children->get(len-1)->_step);
 
-			// FIXME: end might not have a step => should find the first step
-			start->_children->get(len-1)->_step->set_next_step(end->get_first_step(NR_STEP/DEFAULT_STEP_PER_SEQ));
+		// FIXME: end might not have a step => should find the first step
+		start->_children->get(len-1)->_step->set_next_step(end->get_first_step(NR_STEP/DEFAULT_STEP_PER_SEQ));
 
+		dbg::printf("new steps / nodes have been created\n");
+		// create tree
+	
 
-			dbg::printf("new steps / nodes have been created\n");
-			// create tree
+		// clear ui
+		clear_all_long_pushed_ui(t, &_lp_cnt, _lp_ui);
+
+		t->get_led_matrix()->clr_n_restore(errata_btn[start->_mtx_id], BACKGROUND);
+		t->get_led_matrix()->save_n_set(LED_GBR_IDX, errata_btn[start->_mtx_id], BACKGROUND); 
+		t->show_children_node(start);	
 	
-	
-			// clear ui
-			clear_all_long_pushed_ui(t, &_lp_cnt, _lp_ui);
-	
-			t->get_led_matrix()->clr_n_restore(errata_btn[start->_mtx_id], BACKGROUND);
-			t->get_led_matrix()->save_n_set(LED_GBR_IDX, errata_btn[start->_mtx_id], BACKGROUND); 
-			t->show_children_node(start);	
-	
-		} 
-		else {			
+	} 
+	else {			
+		if(n && n->_step){
 			step* s = t->_mtx_to_node[id]->_step;
 			if(s->is_step_active()){
-				s->clr_step_active();
+			s->clr_step_active();
 				t->get_led_matrix()->clr_n_restore(btn_id, BACKGROUND);
 			} else {
 				s->set_step_active();
