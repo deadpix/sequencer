6aceb5fa279eabe7b38ee2bd6f7b629a10b395e4
vincent <vdupre@etri.re.kr>
2019-03-27 17:41:03 +0900

    remove unused functions

--- sequencepter/src/step.cpp
+++ sequencepter/src/step.cpp
@@ -4,12 +4,12 @@
 
 step::step(){
 	flag_active = false;
-	_linked = false;
+//	_linked = false;
 	gate_len_per = 20;
 	_flg_gate = false;
 	_note.velocity = 127;
 	_note.pitch = 37;
-	_gate_bmp = 0x0;
+//	_gate_bmp = 0x0;
 //	_color = LED_OFF_IDX;
 	_color = LED_R_IDX;
 	_clk_def.numerator = 1;
@@ -28,14 +28,16 @@ void step::step_set_note(uint8_t vel, uint16_t pitch){
 bool step::reset_gate(){
 	gate_elapsed = 0;
 	_flg_gate = true;
-	return (_gate_bmp & GATE_UP);
+	return _flg_gate;
+//	return (_gate_bmp & GATE_UP);
 }
 bool step::upd_gate(){
 	bool res = false;
 
-	if( (_gate_bmp & GATE_DW) 
-		&& (gate_elapsed > gate_len_ms) 
-		&& _flg_gate){
+//	if( (_gate_bmp & GATE_DW) 
+//		&& (gate_elapsed > gate_len_ms) 
+//		&& _flg_gate){
+	if( (gate_elapsed > gate_len_ms) && _flg_gate){
 		res = true;
 		_flg_gate = false;
 	}
@@ -52,27 +54,27 @@ bool step::is_step_active(){
 void step::set_step_active(){
 	flag_active = true;
 //	_color = LED_R_IDX;
-	_gate_bmp = GATE_ON;
+//	_gate_bmp = GATE_ON;
 }
 
 void step::clr_step_active(){
 	flag_active = false;
 //	_color = LED_OFF_IDX; 
-	_gate_bmp = GATE_OFF;
+//	_gate_bmp = GATE_OFF;
 }
 
-void step::set_step_up(){
-	flag_active = true;
-	_gate_bmp |= GATE_UP;
-}
-void step::set_step_dw(){
-	flag_active = true;
-	_gate_bmp |= GATE_DW;
-}
-void step::set_step_off(){
-	flag_active = true;
-	_gate_bmp = GATE_OFF;
-}
+//void step::set_step_up(){
+//	flag_active = true;
+//	_gate_bmp |= GATE_UP;
+//}
+//void step::set_step_dw(){
+//	flag_active = true;
+//	_gate_bmp |= GATE_DW;
+//}
+//void step::set_step_off(){
+//	flag_active = true;
+//	_gate_bmp = GATE_OFF;
+//}
 
 void step::set_next_step(step* s){
 	_next = s;
@@ -115,30 +117,30 @@ void step::upd_step_gate_len(uint32_t ms_ref){
 	gate_len_ms = ms_ref * gate_len_per / 100;
 }
 
-uint8_t step::get_step_id(){
-	return step_id;
-}
+//uint8_t step::get_step_id(){
+//	return step_id;
+//}
 
-void step::set_step_id(uint8_t id){
-	step_id = id;
-}
+//void step::set_step_id(uint8_t id){
+//	step_id = id;
+//}
 
 //void step::toogle_step(){
 //	flag_active = !flag_active;
 //}
 
-bool step::step_status(){
-	return flag_active;
-}
+//bool step::step_status(){
+//	return flag_active;
+//}
 
-void step::link_step(){
-	_linked = true;
-//	gate_len_per = 100;
-//	gate_len_ms = 0;
-}
-void step::unlink_step(){
-	_linked = false;
-}
-bool step::is_step_linked(){
-	return _linked;
-}
+//void step::link_step(){
+//	_linked = true;
+////	gate_len_per = 100;
+////	gate_len_ms = 0;
+//}
+//void step::unlink_step(){
+//	_linked = false;
+//}
+//bool step::is_step_linked(){
+//	return _linked;
+//}
--- sequencepter/src/step.h
+++ sequencepter/src/step.h
@@ -15,10 +15,10 @@ class step;
 
 #include "node.h"
 
-#define GATE_UP			(1<<0)
-#define GATE_DW			(1<<1)
-#define GATE_ON			(GATE_UP | GATE_DW)
-#define GATE_OFF		0x0
+//#define GATE_UP			(1<<0)
+//#define GATE_DW			(1<<1)
+//#define GATE_ON			(GATE_UP | GATE_DW)
+//#define GATE_OFF		0x0
 
 struct note {
 	uint8_t  velocity;
@@ -27,10 +27,10 @@ struct note {
 
 class step {
 	private:
-		uint8_t step_id;
+//		uint8_t step_id;
 		volatile bool flag_active;
-		bool _linked;
-		uint8_t _gate_bmp;
+//		bool _linked;
+//		uint8_t _gate_bmp;
 		uint8_t gate_len_per; // percentage
 		uint32_t gate_len_ms; // percentage
 		bool _flg_gate;
@@ -55,15 +55,17 @@ class step {
 	//	void check_gate(uint32_t);
 		bool reset_gate();
 		bool upd_gate();
+	
+		uint8_t get_step_gate_len();
 		void set_step_gate_len(uint32_t ms, uint8_t len);
 		void upd_step_gate_len(uint32_t);
 
 		bool is_step_active();
 		void set_step_active();
 		void clr_step_active();
-		void set_step_up();
-		void set_step_dw();
-		void set_step_off();
+//		void set_step_up();
+//		void set_step_dw();
+//		void set_step_off();
 //		void toogle_step();
 		
 		void set_next_step(step*);
@@ -79,16 +81,15 @@ class step {
 //		void set_clk(clk*);
 //		clk* get_clk();
 
-		uint8_t get_step_gate_len();
-		uint8_t get_step_id();
+//		uint8_t get_step_id();
 		
 		void step_set_note(uint8_t, uint16_t);
 		
-		void set_step_id(uint8_t id);
-		bool step_status();
-		void link_step();
-		void unlink_step();
-		bool is_step_linked();
+//		void set_step_id(uint8_t id);
+//		bool step_status();
+//		void link_step();
+//		void unlink_step();
+//		bool is_step_linked();
 };
 
 #endif
