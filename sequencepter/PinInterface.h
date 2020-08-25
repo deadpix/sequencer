f3b645ef5b6e68a69cb5788774301cbd875492c8
Vincent Dupré <vdupre@gmail.com>
2020-08-25 17:46:00 +0900

    Add file relative to compilation issue

--- sequencepter/PinInterface.h
+++ sequencepter/PinInterface.h
@@ -1,14 +1,6 @@
 #ifndef __PIN_INTERFACE__
 #define __PIN_INTERFACE__
 
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include <Arduino.h>
-#else
-	#include <WProgram.h>
-#endif
-
-
-
 class PinInterface {
 	public:
 		virtual uint8_t readPin(uint8_t pin) = 0;
